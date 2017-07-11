4100261546ce02a5d406b3a6f821c56667c1f69d
Olexandr Shalakhin <olexandr@shalakhin.com>
2017-07-11 16:23:10 +0300

    add setup.cfg and setup.py files

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+
+setup(
+  name = 'fondy',
+  packages = ['fondy'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Python library for the Fondy API',
+  author = 'Olexandr Shalakhin',
+  author_email = 'olexandr@shalakhin.com',
+  url = 'https://github.com/shalakhin/fondy', # use the URL to the github repo
+  download_url = 'https://github.com/shalakhin/fondy/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  classifiers = [],
+)
